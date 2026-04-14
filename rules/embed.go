67cc1b3e54aa2c9e0fbf365b0f19c00c2c78121f
Rost Khaniukov <contact@khaniukov.com>
2026-04-14 22:37:54 +0200

    fix: align CI with recent changes

--- rules/embed.go
+++ rules/embed.go
@@ -2,7 +2,10 @@ package rules
 
 import "embed"
 
+// Policies contains the built-in Rego policy files, embedded at compile time.
+//
 //go:embed policy
 var Policies embed.FS
 
+// PolicyDir is the root directory within Policies containing the .rego files.
 const PolicyDir = "policy"
